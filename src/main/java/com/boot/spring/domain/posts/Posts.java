db6b83efef335c6c08f7787c64ef63511f192c9e
sunjuho <sun_juho@naver.com>
2020-08-11 22:00:41 +0900

    JPA jpaRepository, h2, rest api.

--- src/main/java/com/boot/spring/domain/posts/Posts.java
+++ src/main/java/com/boot/spring/domain/posts/Posts.java
@@ -1,8 +1,8 @@
 package com.boot.spring.domain.posts;
 
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Builder;
 
 import javax.persistence.*;
 
@@ -29,6 +29,12 @@ public class Posts {
         this.title = title;
         this.content = content;
         this.author = author;
+
+    }
+
+    public void update(String title, String content){
+        this.title = title;
+        this.content = content;
 
     }
 }
