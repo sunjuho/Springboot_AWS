6353f9d7715a2c492d09b5222ab29849fe5d595a
sunjuho <sun_juho@naver.com>
2020-08-13 20:56:06 +0900

    OAuth2, 구글 로그인, 권한.

--- src/main/java/com/boot/spring/web/IndexController.java
+++ src/main/java/com/boot/spring/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.boot.spring.web;
 
+import com.boot.spring.config.auth.dto.SessionUser;
 import com.boot.spring.service.PostsService;
 import com.boot.spring.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,15 +9,24 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
+        if(user != null){
+            model.addAttribute("userName", user.getName());
+        }
+
         return "index";
     }
 
@@ -27,7 +37,7 @@ public class IndexController {
     }
 
     @GetMapping("/posts/update/{id}")
-    public String postsUpdate(@PathVariable Long id, Model model){
+    public String postsUpdate(@PathVariable Long id, Model model) {
         PostsResponseDto dto = postsService.findById(id);
 
         model.addAttribute("post", dto);
